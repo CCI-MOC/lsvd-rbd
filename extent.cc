c8088e40a8ce012cfb72c2287ca225175a31e6fc
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-01-24 01:15:29 -0500

    pretty much the final API, I hope. started testing

--- extent.cc
+++ extent.cc
@@ -26,6 +26,11 @@
 
 namespace extmap {
 
+    // we support three map outputs:
+    // LBA        - just an int64_t
+    // obj_offset - sequence number + LBA offset
+    // sector_ptr - points into a memory buffer. ptr increments by 512
+    //
     struct obj_offset {
 	int64_t obj    : 36;
 	int64_t offset : 28;	// 128GB
@@ -53,9 +58,7 @@ namespace extmap {
 	    return (obj_offset){.obj = obj, .offset = offset + val};
 	}
 	int operator-(const obj_offset other) {
-	    if (obj != other.obj)
-		return 0;
-	    return offset - other.offset;
+	    return offset - other.offset; // meaningless if obj != other.obj
 	}
     };
 
@@ -90,6 +93,7 @@ namespace extmap {
     
     // These are the three map types we support. There's probably a way to 
     // do this with a template, but I don't think it's worth the effort
+    // these are the actual structures stored in the map
     //
     struct _lba2buf {
 	int64_t    base : 40;
@@ -109,6 +113,9 @@ namespace extmap {
 	obj_offset ptr;
     };
 
+    // Here's the wrapper around those types, so that we can use the same
+    // map logic for all three of them
+    //
     template <class T, class T_in, class T_out> 
     struct _extent {
 	T s;
@@ -122,9 +129,16 @@ namespace extmap {
 	    s.base = _base;
 	}
 	_extent() {}
+
+	// extents all get implemented as [base .. base+len) but the logic 
+	// using them a lot easier if we work with [base .. limit)
+	// 
 	T_in base(void) { return s.base; }
 	T_in limit(void) { return s.base + s.len; }
 	void limit(T_in _limit) { s.len = _limit - s.base; }
+
+	// if we change the base, we need to update the pointer by the same amount
+	//
 	void base(T_in _base) {
 	    auto delta = _base - s.base;
 	    s.ptr += delta;
@@ -140,8 +154,9 @@ namespace extmap {
 	    return s.base < other.s.base;
 	}
 
+	// can't use a bitfield directly in make_tuple - need +0
 	std::tuple<T_in, T_in, T_out> vals(void) {
-	    return std::make_tuple(s.base, s.base+s.len, s.ptr);
+	    return std::make_tuple(s.base+0, s.base+s.len, s.ptr);
 	}
 
 	std::tuple<T_in, T_in, T_out> vals(T_in _base, T_in _limit) {
@@ -160,37 +175,26 @@ namespace extmap {
     typedef _extent<_obj2lba,obj_offset,int64_t> obj2lba;
     typedef _extent<_lba2obj,int64_t,obj_offset> lba2obj;
 
-    template <class T, class T_in>
-    struct x_pair {
-	mutable T_in max;
-	std::vector<T> *list;
-    public:
-	bool operator<(const x_pair &other) const {
-	    return max < other.max;
-	}
-    };
-
+    // an extent map with entries of type T, which map from T_in to T_out
+    //
     template <class T, class T_in, class T_out>
     struct extmap {
 	static const int _load = 256;
 
+    public:
 	typedef std::vector<T>      extent_vector;
 	std::vector<extent_vector*> lists;
 	std::vector<T_in>           maxes;
-	
-	int count;
-	
-	// slices off [len]..[end] and returns it
+	int                         count;
+
+	// debug code
 	//
-	static std::vector<T> *slice(std::vector<T> *A, int len) {
-	    auto half = new std::vector<T>();
-	    half->reserve(_load);
-	    for (auto it = A->begin()+len; it != A->end(); it++)
-		half->push_back(*it);
-	    A->resize(len);
-	    return half;
+	void verify_max(void) {
 	}
-	
+
+	// iterator gets used both internally and externally
+	// (returned by lookup function)
+	//
 	class iterator {
 	public:
 	    extmap *m;
@@ -251,6 +255,10 @@ namespace extmap {
 		return *this;
 	    }
 
+	    // it+1 / it-1 is useful in several cases. We never need to
+	    // add/subtract more than 1, so the overhead of recursive solution
+	    // isn't a big deal
+	    //
 	    iterator operator-(std::ptrdiff_t n) {
 		if (n > 1)
 		    return (*this + 1) + (n-1);
@@ -260,7 +268,6 @@ namespace extmap {
 		    return iterator(m, i-1, m->lists[i-1]->end() - 1);
 		return iterator(m, i, it-1);
 	    }
-
 	    iterator operator+(std::ptrdiff_t n) {
 		if (n > 1)
 		   return (*this + 1) + (n-1);
@@ -273,9 +280,9 @@ namespace extmap {
 		return iterator(m, i, it+1);
 	    }
 
-	    bool operator<(T_in val) const {
-		return (*this == m->end() || it->base < val);
-	    }
+//	    bool operator<(T_in val) const {
+//		return (*this == m->end() || it->base < val);
+//	    }
 	};
 	
 	iterator begin() {
@@ -292,7 +299,7 @@ namespace extmap {
 	}
 	
 	iterator lower_bound(T_in base) {
-	    if (count == 0)	// should never get called
+	    if (lists.size() == 0) 	// should never get called in this case?
 		return end();
 
 	    // search maxes to find the list containing @base
@@ -304,13 +311,13 @@ namespace extmap {
 	    if (max_iter == maxes.end())
 		return end();
 
-	    // find lowest entry with @base >= @base
+	    // find lowest entry with base >= @base
 	    //
 	    auto i = max_iter - maxes.begin();
 	    T _key(base);
 	    auto list_iter = std::lower_bound(lists[i]->begin(), lists[i]->end(), _key);
 
-	    // whoops, could match the entry before this...
+	    // whoops, previous entry could have limit > @base...
 	    //
 	    if (list_iter != lists[i]->begin()) {
 		auto prev = list_iter - 1;
@@ -325,11 +332,29 @@ namespace extmap {
 
 	    return iterator(this, i, list_iter);
 	}
+
+	// Following logic from Python sorted containers, by Grant Jenks.
+	// https://github.com/grantjenks/python-sortedcontainers
+	// sortedlist.py in particular
+	//
 	
+	// Python-style list slicing - remove [len]..[end] and return it
+	//
+	static std::vector<T> *_slice(std::vector<T> *A, int len) {
+	    auto half = new std::vector<T>();
+	    half->reserve(_load);
+	    for (auto it = A->begin()+len; it != A->end(); it++)
+		half->push_back(*it);
+	    A->resize(len);
+	    return half;
+	}
+
+	// sortedlist._expand(self, pos)
+	//
 	iterator _expand(iterator it) {
 	    if (lists[it.i]->size() >= _load * 2) {
 		int j = it.it - lists[it.i]->begin();
-		auto half = slice(lists[it.i], _load);
+		auto half = _slice(lists[it.i], _load);
 		maxes[it.i] = lists[it.i]->back().limit();
 		lists.insert(lists.begin()+it.i+1, half);
 		maxes.insert(maxes.begin()+it.i+1, half->back().limit());
@@ -342,19 +367,16 @@ namespace extmap {
 	    return it;
 	}
 
-	void verify_max(void) {
-	}
-
-	// inserts just before iterator 'it'
-	// returns pointer to inserted value
-	iterator insert(iterator it, T _e) {
+	// insert just before iterator 'it', return pointer to inserted value
+	//
+	iterator _insert(iterator it, T _e) {
 	    it.it = lists[it.i]->insert(it.it, _e);
 	    maxes[it.i] = lists[it.i]->back().limit();
 	    count++;
 	    return _expand(it);
 	}
 	
-	// corresponds to sortedlist._delete
+	// sortedlist._delete
 	//
 	iterator _erase(iterator it) {
 	    it.it = lists[it.i]->erase(it.it);
@@ -389,18 +411,27 @@ namespace extmap {
 	    }
 	    return it;
 	}
-	
+
+	// helper - can we merge these two extents?
+	//
 	static bool adjacent(T left, T right) {
 	    return left.limit() == right.base() &&
 		left.s.ptr + (left.limit() - left.base()) == right.s.ptr;
 	}
 
+	// update the map
+	//    trim=T : just unmap [@base..@limit)
+	//    trim=F : replace with [@base..@limit) -> [@e..@e+len)
+	// removed extents are returned in del[] (if non-null) for GC tracking
+	//
 	void _update(T_in base, T_in limit, T_out e, bool trim, extent_vector *del) {
 	    //= {.base = base, .limit = limit, .ext = e};
 	    T _e(base, limit-base, e);
 
 	    verify_max();
 
+	    // special case inserting the first entry
+	    //
 	    if (maxes.size() == 0) {
 		auto vec = new extent_vector();
 		vec->reserve(_load);
@@ -413,7 +444,10 @@ namespace extmap {
 	    // TODO - old fixit() function?
 	    verify_max();
 
+	    // find the first extent with base >= @base
+	    //
 	    auto it = lower_bound(base);
+
 	    if (it != end()) {
 		// we bisect an extent
 		//   [-----------------]       *it          _new
@@ -431,7 +465,7 @@ namespace extmap {
 			   it->s.ptr + (limit - it->base()));
 		    it->limit(base);
 		    maxes[it.i] = lists[it.i]->back().limit();
-		    it = insert(it+1, _new);
+		    it = _insert(it+1, _new);
 		    verify_max();
 		}
 		
@@ -484,9 +518,13 @@ namespace extmap {
 	    if (!trim) {
 		auto prev = it-1;
 		if (it != begin() && adjacent(*prev, _e)) {
+		    // we can merge with the previous extent
+		    //
 		    prev->limit(limit);
 		    maxes[prev.i] = lists[prev.i]->back().limit();
 		    if (it != end() && adjacent(*prev, *it)) {
+			// we plug a hole, and can merge with the next extent
+			//
 			prev->limit(it->limit());
 			maxes[prev.i] = lists[prev.i]->back().limit();
 			_erase(it);
@@ -495,22 +533,42 @@ namespace extmap {
 		    }
 		}
 		else if (it != end() && adjacent(_e, *it)) {
+		    // we can merge with the next extent
+		    //
 		    it->s.ptr += (base - limit); // subtract
 		    it->base(base);
 		}
 		else {
-		    insert(it, _e);
+		    // no merging, just insert the damn thing
+		    //
+		    _insert(it, _e);
 		    verify_max();
 		}
 	    }
 	}
 
-    public:
-	// returns iterator pointing to one of:
+//    public:
+	int size() {
+	    return count;
+	}
+
+	int capacity() {
+	    int sum = 0;
+	    for (auto list : lists)
+		sum += list->capacity();
+	    return sum;
+	}
+	
+	// lookup - returns iterator pointing to one of:
 	// - extent containing @base
 	// - lowest extent with base > @base
 	// - end()
-	iterator _lookup(T_in base) {
+
+	// usage:
+	// for (auto it = lookup(base); it != end() && it->base < limit; it++) 
+	//    auto [base, limit, ptr] = it->vals(base, limit);
+	//
+	iterator lookup(T_in base) {
 	    auto it = lower_bound(base);
 	    if (it == end())
 		return it;
@@ -529,27 +587,22 @@ namespace extmap {
 	    }
 	    return it;
 	}
-	
-	int size() {
-	    return count;
-	}
 
-	int capacity() {
-	    int sum = 0;
-	    for (auto list : lists)
-		sum += list->capacity();
-	    return sum;
+	// various ways of calling _update...
+	//
+	void update(T_in base, T_in limit, T_out e, extent_vector *del) {
+	    _update(base, limit, e, false, del);
 	}
-
 	void update(T_in base, T_in limit, T_out e) {
 	    _update(base, limit, e, false, nullptr);
 	}
 
+	void trim(T_in base, T_in limit, T_out e, extent_vector *del) {
+	    _update(base, limit, e, true, del);
+	}
 	void trim(T_in base, T_in limit, T_out e) {
 	    _update(base, limit, e, true, nullptr);
 	}
-
-	
     };
 
     // template <class T, class T_in, class T_out>
