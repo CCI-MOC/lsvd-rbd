4921c16c431576ba8c1d8457abe0d8fd60cb50a0
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-01-25 16:19:34 -0500

    a few notes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # new-lsvd
+
+thread pool:
+
+took a stab at using https://github.com/DeveloperPaul123/thread-pool,
+which seems to be a nice header-only thread pool, but it's a pain in
+the ass to compile.
+
+Instead think I'll use:
+https://github.com/fbastos1/thread_pool_cpp17
+
+from
+https://codereview.stackexchange.com/questions/221626/c17-thread-pool
--- lsvd.cc
+++ lsvd.cc
@@ -9,7 +9,21 @@ std::stack<char*> free_bufs;
 char *current_buf, *current_ptr;
 size_t buf_size;
 
-#include "thread_pool.h"
+void write(int64_t lba, char *buf, int len)
+{
+    if (len + (current_ptr - current_buf) > buf_size) {
+	char *buf = current_buf;
+	size_t len = current_ptr - current_buf;
+	if (free_bufs.size() == 0)
+	    current_buf = malloc(buf_size);
+	else {
+	    current_buf = free_bufs.top();
+	    free_bufs.pop();	// why can't C++ stacks work right?
+	}
+	current_ptr = current_buf;
+    }
+}
+
 
 int main(int argc, char **argv)
 {
