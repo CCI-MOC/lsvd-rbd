b5f1185822abb92e939fbebca437355ed1116aa7
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-03-12 03:07:50 +0000

    print out UUIDs

--- cache.py
+++ cache.py
@@ -4,7 +4,7 @@ import sys
 import os
 import lsvd
 import test3 as t3
-
+import uuid
 
 def prettyprint(p, insns):
     for field,fmt in insns:
@@ -17,15 +17,20 @@ def prettyprint(p, insns):
         elif type(fmt) == dict:
             print(field, pad, fmt[x])
 
+fmt_uuid = lambda u: uuid.UUID(bytes=bytes(u[:]))
+
 fieldnames = dict({(10, 'DATA'),(11, 'CKPT'),(12, 'PAD'),(13, 'SUPER'),
                        (14, 'W_SUPER'),(15, 'R_SUPER')})
 magic = lambda x: 'ok' if x == lsvd.LSVD_MAGIC else '**BAD**'
-hdr_pp = [['magic', magic], ['type', fieldnames], ['write_super', '%d'], ['read_super', '%d']]
+hdr_pp = [['magic', magic], ['type', fieldnames], ['vol_uuid', fmt_uuid], ['write_super', '%d'], ['read_super', '%d']]
 
-wsup_pp = [['magic', magic], ['type', fieldnames], ['seq', '%d'], ['base', '%d'], ['limit', '%d'],
+wsup_pp = [['magic', magic], ['type', fieldnames], ['vol_uuid', fmt_uuid], ['seq', '%d'], ['base', '%d'], ['limit', '%d'],
                ['next', '%d'], ['oldest', '%d']]
 
-rsup_pp = [["magic", magic], ["type", fieldnames], ["unit_size", '%d'], ["base", '%d'], ["units", '%d']]
+rsup_pp = [["magic", magic], ["type", fieldnames], ['vol_uuid', fmt_uuid], ["unit_size", '%d'], 
+               ["base", '%d'], ["units", '%d'], ["map_start", '%d'],["map_blocks", '%d'],
+               ["bitmap_start", '%d'], ["bitmap_blocks", '%d'], ["evict_type", '%d'],
+               ["evict_start", '%d'], ["evict_blocks", '%d']]
 
 fd = os.open(sys.argv[1], os.O_RDONLY)
 super = t3.c_super(fd)
