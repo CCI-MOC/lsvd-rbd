bac2fd57105507dd04fb71649acc4b760d6e6da4
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-03-12 03:06:46 +0000

    fixes to mkcache on large devices or files

--- mkcache.py
+++ mkcache.py
@@ -10,40 +10,54 @@ import uuid
 # 1. variable size
 # 3. option to not write cache pages
 
-def mkcache(name, uuid=b'\0'*16):
-    fp = open(name, 'wb')
+def div_round_up(n, m):
+    return (n + m - 1) // m
+
+def mkcache(name, uuid=b'\0'*16, write_zeros=True, wblks=125, rblks=16*16):
+    fd = os.open(name, os.O_WRONLY | os.O_CREAT, 0o777)
 
     sup = lsvd.j_super(magic=lsvd.LSVD_MAGIC, type=lsvd.LSVD_J_SUPER,
                        write_super=1, read_super=2, backend_type=lsvd.LSVD_BE_FILE)
     sup.vol_uuid[:] = uuid
     data = bytearray() + sup
     data += b'\0' * (4096-len(data))
-    fp.write(data) # page 0
+    os.write(fd, data) # page 0
 
     # write cache has 125 single-page entries. default: map_blocks = map_entries = 0
     wsup = lsvd.j_write_super(magic=lsvd.LSVD_MAGIC, type=lsvd.LSVD_J_W_SUPER,
-                              seq=1, base=3, limit=128, next=3, oldest=3)
+                              seq=1, base=3, limit=wblks+3, next=3, oldest=3)
     wsup.vol_uuid[:] = uuid
     data = bytearray() + wsup
     data += b'\0' * (4096-len(data))
-    fp.write(data) # page 1
+    os.write(fd, data) # page 1
 
+    rbase = wblks+3
+    units = rblks // 16
+    map_blks = div_round_up(units*lsvd.sizeof_obj_offset, 4096)
+    print('map blocks', map_blks)
+    bitmap_blks = div_round_up(units*2, 4096)
+    print('bitmap blocks', bitmap_blks)
+    
     # 1 page for map, 1 page for bitmap
     rsup = lsvd.j_read_super(magic=lsvd.LSVD_MAGIC, type=lsvd.LSVD_J_R_SUPER,
-                                unit_size=128, base=130, units=16, map_start=128,
-                                map_blocks=1, bitmap_start=129, bitmap_blocks=1)
+                                unit_size=128, units=units,
+                                map_start=rbase, map_blocks=map_blks,
+                                bitmap_start=rbase+map_blks, bitmap_blocks=bitmap_blks,
+                                base=rbase+map_blks+bitmap_blks)
+
     rsup.vol_uuid[:] = uuid
     data = bytearray() + rsup
     data += b'\0' * (4096-len(data))
-    fp.write(data) # page 2
+    os.write(fd, data) # page 2
 
     # zeros are invalid entries for cache map/bitmap
     # 130 + 16*(64KB/4KB) = 386
-    
-    data = bytearray(b'\0'*4096)
-    for i in range(2,386):
-        fp.write(data)
-    fp.close()
+
+    if (write_zeros):
+        data = bytearray(b'\0'*4096)
+        for i in range(3 + wblks + map_blks + bitmap_blks + rblks):
+            os.write(fd, data)
+    os.close(fd)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='initialize LSVD cache')
@@ -53,5 +67,17 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     uuid = uuid.UUID(args.uuid).bytes
-    mkcache(args.device, uuid)
+
+    if os.access(args.device, os.F_OK):
+        s = os.stat(args.device)
+        bytes = s.st_size
+        pages = bytes // 4096
+        r_units = int(0.75*pages) // 16
+        r_pages = r_units * 16
+        oh = div_round_up(r_units*(2+lsvd.sizeof_obj_offset), 4096)
+        w_pages = pages - oh - r_pages - 3
+    
+        mkcache(args.device, uuid, write_zeros=False, wblks=w_pages, rblks=r_pages)
+    else:
+        mkcache(args.device, uuid)
 
