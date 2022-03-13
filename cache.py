f739263d4be8d59564b9f7dc8e8dc061e354edff
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-03-13 14:39:21 +0000

    handle mangled SSD superblocks

--- cache.py
+++ cache.py
@@ -22,9 +22,13 @@ fmt_uuid = lambda u: uuid.UUID(bytes=bytes(u[:]))
 fieldnames = dict({(10, 'DATA'),(11, 'CKPT'),(12, 'PAD'),(13, 'SUPER'),
                        (14, 'W_SUPER'),(15, 'R_SUPER')})
 magic = lambda x: 'ok' if x == lsvd.LSVD_MAGIC else '**BAD**'
-hdr_pp = [['magic', magic], ['type', fieldnames], ['vol_uuid', fmt_uuid], ['write_super', '%d'], ['read_super', '%d']]
+blk_fmt = lambda x: '%d%s' % (x, '' if x < npages else ' *INVALID*')
 
-wsup_pp = [['magic', magic], ['type', fieldnames], ['vol_uuid', fmt_uuid], ['seq', '%d'], ['base', '%d'], ['limit', '%d'],
+hdr_pp = [['magic', magic], ['type', fieldnames], ['vol_uuid', fmt_uuid],
+              ['write_super', blk_fmt], ['read_super', blk_fmt]]
+
+wsup_pp = [['magic', magic], ['type', fieldnames], ['vol_uuid', fmt_uuid],
+               ['seq', '%d'], ['base', '%d'], ['limit', '%d'],
                ['next', '%d'], ['oldest', '%d']]
 
 rsup_pp = [["magic", magic], ["type", fieldnames], ['vol_uuid', fmt_uuid], ["unit_size", '%d'], 
@@ -33,16 +37,20 @@ rsup_pp = [["magic", magic], ["type", fieldnames], ['vol_uuid', fmt_uuid], ["uni
                ["evict_start", '%d'], ["evict_blocks", '%d']]
 
 fd = os.open(sys.argv[1], os.O_RDONLY)
+npages = os.fstat(fd).st_size // 4096
+
 super = t3.c_super(fd)
 print('superblock: (0)')
 prettyprint(super, hdr_pp)
-w_super = t3.c_w_super(fd, super.write_super)
-print('\nwrite superblock: (%d)' % super.write_super)
-prettyprint(w_super, wsup_pp)
+print('\nwrite superblock: (%s)' % blk_fmt(super.write_super))
+if super.write_super < npages:
+    w_super = t3.c_w_super(fd, super.write_super)
+    prettyprint(w_super, wsup_pp)
 
-r_super = t3.c_r_super(fd, super.read_super)
-print('\nread superblock: (%d)' % super.read_super)
-prettyprint(r_super, rsup_pp)
+print('\nread superblock: (%s)' % blk_fmt(super.read_super))
+if super.read_super < npages:
+    r_super = t3.c_r_super(fd, super.read_super)
+    prettyprint(r_super, rsup_pp)
 
 def read_exts(b, npgs, n):
     buf = os.pread(fd, npgs*4096, b*4096)
@@ -50,21 +58,22 @@ def read_exts(b, npgs, n):
     e = (lsvd.j_map_extent*n).from_buffer(bytearray(buf[0:bytes]))
     return [(_.lba, _.len, _.page) for _ in e]
 
-b = w_super.oldest
-while True:
-    [j,e] = t3.c_hdr(fd, b)
-#    if j.type != lsvd.LSVD_J_DATA:
-#        break
-    if j.magic != lsvd.LSVD_MAGIC:
-        break
-    if j.type == lsvd.LSVD_J_CKPT:
-        print('\ncheckpoint:', b)
-        e = read_exts(b+1, j.len - 1, w_super.map_entries)
-        print(' '.join(['(%d+%d->%d)' % _ for _ in e]))
-    else:
-        h_pp = [["magic", magic], ["type", fieldnames], ["seq", '%d'], ["len", '%d']]
-        print('\ndata: (%d)' % b)
-        prettyprint(j, h_pp)
-        print('extents    :', ' '.join(['%d+%d' % (_.lba,_.len) for _ in e]))
-    b = b + j.len
+if super.write_super < npages:
+    b = w_super.oldest
+    while True:
+        [j,e] = t3.c_hdr(fd, b)
+    #    if j.type != lsvd.LSVD_J_DATA:
+    #        break
+        if j.magic != lsvd.LSVD_MAGIC:
+            break
+        if j.type == lsvd.LSVD_J_CKPT:
+            print('\ncheckpoint:', b)
+            e = read_exts(b+1, j.len - 1, w_super.map_entries)
+            print(' '.join(['(%d+%d->%d)' % _ for _ in e]))
+        else:
+            h_pp = [["magic", magic], ["type", fieldnames], ["seq", '%d'], ["len", '%d']]
+            print('\ndata: (%d)' % b)
+            prettyprint(j, h_pp)
+            print('extents    :', ' '.join(['%d+%d' % (_.lba,_.len) for _ in e]))
+        b = b + j.len
 
