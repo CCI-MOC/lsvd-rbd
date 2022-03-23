620a810aa146691e2b532987c0b7dd9a60b73972
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-03-23 20:47:47 +0000

    updated bdus shell to use fake rbd functions

--- bdus.c
+++ bdus.c
@@ -1,11 +1,18 @@
 #include <bdus.h>
 #include <stdio.h>
 
-extern int c_read(char*, uint64_t, uint32_t, struct bdus_ctx*);
-extern int c_write(const char*, uint64_t, uint32_t, struct bdus_ctx*);
-extern ssize_t c_init(char *name, int n);
-extern int c_flush(struct bdus_ctx* ctx);
-extern void c_shutdown(void);
+//typedef void* rados_ioctx_t;
+//typedef void* rbd_image_t;
+
+#include "fake_rbd.h"
+
+extern int rbd_open(rados_ioctx_t io, const char *name, rbd_image_t *image,
+                    const char *snap_name);
+extern int rbd_close(rbd_image_t image);
+
+extern int rbd_read(rbd_image_t image, uint64_t off, size_t len, char *buf);
+extern int rbd_write(rbd_image_t image, uint64_t off, size_t len, char *buf);
+extern int rbd_flush(rbd_image_t image);
 
 int do_init(struct bdus_ctx* ctx)
 {
@@ -14,20 +21,32 @@ int do_init(struct bdus_ctx* ctx)
 
 int do_terminate(struct bdus_ctx* ctx)
 {
-    c_shutdown();
-    return 0;
+    rbd_image_t img = ctx->private_data;
+    return rbd_close(img);
+}
+
+int do_read(char *buffer, uint64_t offset, uint32_t size, struct bdus_ctx *ctx)
+{
+    rbd_image_t img = ctx->private_data;
+    return rbd_read(img, offset, size, buffer);
+}
+
+int do_write(const char *buffer, uint64_t offset, uint32_t size, struct bdus_ctx *ctx)
+{
+    rbd_image_t img = ctx->private_data;
+    return rbd_write(img, offset, size, buffer);
 }
 
 int do_flush(struct bdus_ctx *ctx)
 {
-    c_flush(ctx);
-    return 0;
+    rbd_image_t img = ctx->private_data;
+    return rbd_flush(img);
 }
 
 static const struct bdus_ops device_ops =
 {
-    .read       = c_read,
-    .write      = c_write,
+    .read       = do_read,
+    .write      = do_write,
     .initialize = do_init,
     .flush      = do_flush,
     .terminate  = do_terminate,
@@ -42,12 +61,16 @@ static struct bdus_attrs device_attrs =
 
 int main(int argc, char **argv)
 {
-    ssize_t val = c_init(argv[1], 1);
-    if (val < 0)
-        exit(1);
-    device_attrs.size = val;
+    rbd_image_t img;
+    int rv = rbd_open(NULL, argv[1], &img, NULL);
+    if (rv < 0)
+        fprintf(stderr, "failed to open\n"), exit(1);
     
-    bool success = bdus_run(&device_ops, &device_attrs, NULL);
+    rbd_image_info_t info;
+    rbd_stat(img, &info, sizeof(info));
+    device_attrs.size = info.size;
+    
+    bool success = bdus_run(&device_ops, &device_attrs, img);
     if (!success)
         fprintf(stderr, "Error: %s\n", bdus_get_error_message());
     return success ? 0 : 1;
