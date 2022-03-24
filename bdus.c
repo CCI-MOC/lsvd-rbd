64105a88ee49aee10d96c7a7d40b335b3440d6fb
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-03-24 16:25:20 +0000

    added argp parsing with threads parameter and separate SSD, object prefix args

--- bdus.c
+++ bdus.c
@@ -1,8 +1,6 @@
 #include <bdus.h>
 #include <stdio.h>
-
-//typedef void* rados_ioctx_t;
-//typedef void* rbd_image_t;
+#include <argp.h>
 
 #include "fake_rbd.h"
 
@@ -11,7 +9,7 @@ extern int rbd_open(rados_ioctx_t io, const char *name, rbd_image_t *image,
 extern int rbd_close(rbd_image_t image);
 
 extern int rbd_read(rbd_image_t image, uint64_t off, size_t len, char *buf);
-extern int rbd_write(rbd_image_t image, uint64_t off, size_t len, char *buf);
+extern int rbd_write(rbd_image_t image, uint64_t off, size_t len, const char *buf);
 extern int rbd_flush(rbd_image_t image);
 
 int do_init(struct bdus_ctx* ctx)
@@ -57,13 +55,50 @@ static struct bdus_attrs device_attrs =
     .size               = 1 << 30, // 1 GiB
     .logical_block_size = 512,
     .dont_daemonize     = 1,
-    .max_concurrent_callbacks = 2,
+    .max_concurrent_callbacks = 0,
 };
+
+
+static char args_doc[] = "SSD OBJ_PREFIX";
+static struct argp_option options[] = {
+    {"threads",      't', "THREADS",   0, "max number of threads"},
+    {0},
+};
+char *ssd, *prefix;
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+    switch (key) {
+    case 't':
+        device_attrs.max_concurrent_callbacks = atoi(arg);
+        break;
+    case ARGP_KEY_ARG:
+        if (ssd == NULL)
+            ssd = arg;
+        else if (prefix == NULL)
+            prefix = arg;
+        else
+            return ARGP_ERR_UNKNOWN;
+        break;
+    case ARGP_KEY_END:
+        if (!ssd || !prefix)
+            argp_usage(state);
+        break;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+static struct argp argp = { options, parse_opt, NULL, args_doc};
 
 int main(int argc, char **argv)
 {
+    argp_parse (&argp, argc, argv, ARGP_LONG_ONLY, 0, NULL);
+    char rbd_name[128];
+    sprintf(rbd_name, "%s,%s", ssd, prefix);
+    
     rbd_image_t img;
-    int rv = rbd_open(NULL, argv[1], &img, NULL);
+    int rv = rbd_open(NULL, rbd_name, &img, NULL);
     if (rv < 0)
         fprintf(stderr, "failed to open\n"), exit(1);
     
