c2c9239cda3b47bac7aef0d43c0d40284dd23b58
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-09-28 22:16:43 +0000

    removed contents before deleting

--- misc_cache.cc
+++ misc_cache.cc
@@ -22,36 +22,4 @@
 namespace fs = std::experimental::filesystem;
 
 #include "misc_cache.h"
-/*    template<class T>
-    bool thread_pool<T>::get_locked(std::unique_lock<std::mutex> &lk, T &val) {
-        while (running && q.empty())
-            cv.wait(lk);
-        if (!running)
-            return false;
-        val = q.front();
-        q.pop();
-        return val;
-    }
-    template<class T>
-    void thread_pool<T>::put_locked(T work) {
-        q.push(work);
-        cv.notify_one();
-    }
-    template<class T>
-    void thread_pool<T>::put(T work) {
-        std::unique_lock<std::mutex> lk(*m);
-        put_locked(work);
-    }
-*/
-/*    template<class T> 
-    void decode_offset_len(char *buf, size_t offset, size_t len, std::vector<T> &vals) {
-        T *p = (T*)(buf + offset), *end = (T*)(buf + offset + len);
-        for (; p < end; p++)
-            vals.push_back(*p);
-    }
-*/
 
-
-    void throw_fs_error(std::string msg) {
-	throw fs::filesystem_error(msg, std::error_code(errno, std::system_category()));
-    }
