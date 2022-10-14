2ac8b1f2d6bb9e296760707e029f29d80d13499b
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-10-14 05:49:32 +0000

    fix for handling corrupt data journal entries

--- test3.py
+++ test3.py
@@ -49,8 +49,10 @@ def c_hdr(fd, blk):
     b = bytearray(os.pread(fd, 4096, 4096*blk))
     o2 = lsvd.sizeof_j_hdr
     h = lsvd.j_hdr.from_buffer(b[0:o2])
-    n_exts = h.extent_len // lsvd.sizeof_j_extent
-    e = (lsvd.j_extent*n_exts).from_buffer(b[o2:o2+h.extent_len])
+    e = []
+    if h.magic == lsvd.LSVD_MAGIC:
+        n_exts = h.extent_len // lsvd.sizeof_j_extent
+        e = (lsvd.j_extent*n_exts).from_buffer(b[o2:o2+h.extent_len])
     return [h, e]
 
 def restart():
