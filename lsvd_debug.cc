14461a5be737d2e77350f3abdd71f4558cb53ff7
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-10-26 21:21:50 +0000

    various debug code for trying to get it to work with qemu

--- lsvd_debug.cc
+++ lsvd_debug.cc
@@ -258,6 +258,7 @@ char *logbuf, *p_log, *end_log;
 #include <stdarg.h>
 std::mutex m;
 void do_log(const char *fmt, ...) {
+    return;
     std::unique_lock lk(m);
     if (!logbuf) {
 	size_t sz = 64*1024;
@@ -452,3 +453,200 @@ extern "C" void fakemap_reset(_dbg *d)
 {
     d->obj_map.reset();
 }
+
+class aio_vreq {
+public:
+    enum lsvd_op op;
+    smartiov   iovs;
+    char      *buf;
+    bool       done = false;
+    std::mutex m;
+    std::condition_variable cv;
+
+    aio_vreq(enum lsvd_op op_, char *buf_, size_t *sizes, int n) {
+	op = op_;
+	buf = buf_;
+	for (int i = 0; i < n; i++) {
+	    auto ptr = aligned_alloc(512, sizes[i]*512);
+	    iovs.push_back((iovec){ptr, sizes[i]*512});
+	}
+    }
+    ~aio_vreq() {
+	for (int i = 0; i < iovs.size(); i++)
+	    free(iovs[i].iov_base);
+    }
+};
+
+void aio_vreq_cb(rbd_completion_t c, void *arg) {
+    auto r = (aio_vreq*)arg;
+    std::unique_lock lk(r->m);
+    r->done = true;
+    r->cv.notify_one();
+    rbd_aio_release(c);
+}    
+
+extern "C" void *launch_rbd_aio_writev(rbd_image_t image, uint64_t offset,
+				      char *buf, size_t *sizes, int nsizes) {
+    auto req = new aio_vreq(OP_WRITE, buf, sizes, nsizes);
+    req->iovs.copy_in(buf);
+    auto [iov,niov] = req->iovs.c_iov();
+    
+    rbd_completion_t c;
+    rbd_aio_create_completion((void*)req, aio_vreq_cb, &c);
+    rbd_aio_writev(image, iov, niov, offset, c);
+
+    return (void*)req;
+}
+extern "C" void wait_rbd_aio_writev(void *r) {
+    auto req = (aio_vreq*)r;
+    std::unique_lock lk(req->m);
+    while (!req->done)
+	req->cv.wait(lk);
+    lk.unlock();
+    delete req;
+}
+ 
+extern "C" void *launch_rbd_aio_readv(rbd_image_t image, uint64_t offset,
+				      char *buf, size_t *sizes, int nsizes) {
+    auto req = new aio_vreq(OP_READ, buf, sizes, nsizes);
+    auto [iov,niov] = req->iovs.c_iov();
+    
+    rbd_completion_t c;
+    rbd_aio_create_completion((void*)req, aio_vreq_cb, &c);
+    rbd_aio_readv(image, iov, niov, offset, c);
+
+    return (void*)req;
+}
+
+extern "C" void wait_rbd_aio_readv(void *r) {
+    auto req = (aio_vreq*)r;
+    std::unique_lock lk(req->m);
+    while (!req->done)
+	req->cv.wait(lk);
+    req->iovs.copy_out(req->buf);
+    lk.unlock();
+    delete req;
+}
+
+struct waiter {
+    std::mutex m;
+    std::condition_variable cv;
+    bool done = false;
+};
+
+void waiter_cb(rbd_completion_t c, void *arg) {
+    auto w = (waiter*) arg;
+    std::unique_lock lk(w->m);
+    w->done = true;
+    w->cv.notify_one();
+    rbd_aio_release(c);
+}
+
+extern "C" int do_rbd_aio_writev(rbd_image_t image, const iovec *iov,
+		      int iovcnt, uint64_t offset) {
+    rbd_completion_t c;
+    waiter w;
+    
+    rbd_aio_create_completion((void*)&w, waiter_cb, &c);
+    rbd_aio_writev(image, iov, iovcnt, offset, c);
+    std::unique_lock lk(w.m);
+    while (!w.done)
+	w.cv.wait(lk);
+    return 0;
+}
+
+extern "C" int do_rbd_aio_readv(rbd_image_t image, const iovec *iov,
+		      int iovcnt, uint64_t offset) {
+    rbd_completion_t c;
+    waiter w;
+    
+    rbd_aio_create_completion((void*)&w, waiter_cb, &c);
+    rbd_aio_readv(image, iov, iovcnt, offset, c);
+    std::unique_lock lk(w.m);
+    while (!w.done)
+	w.cv.wait(lk);
+    return 0;
+}
+
+/* random run-time debugging stuff, not used at the moment...
+ */
+#if 0
+#include <zlib.h>
+static std::map<int,uint32_t> sector_crc;
+char zbuf[512];
+static std::mutex zm;
+
+static void add_crc(sector_t sector, iovec *iov, int niovs) {
+    std::unique_lock lk(zm);
+    for (int i = 0; i < niovs; i++) {
+	for (size_t j = 0; j < iov[i].iov_len; j += 512) {
+	    const unsigned char *ptr = j + (unsigned char*)iov[i].iov_base;
+	    unsigned c = sector_crc[sector] = (uint32_t)crc32(0, ptr, 512);
+	    //printf("[%d] = %08x\n", (int)sector, sector_crc[sector]);
+	    if (sector == 6923 || sector == 0)
+		do_log("%ld = %08x\n", sector, c);
+	    sector++;
+	}
+    }
+}
+
+void check_crc(sector_t sector, iovec *iov, int niovs, const char *msg) {
+    std::unique_lock lk(zm);
+    for (int i = 0; i < niovs; i++) {
+	for (size_t j = 0; j < iov[i].iov_len; j += 512) {
+	    const unsigned char *ptr = j + (unsigned char*)iov[i].iov_base;
+	    if (sector_crc.find(sector) == sector_crc.end()) {
+		//assert(memcmp(ptr, zbuf, 512) == 0);
+		if (memcmp(ptr, zbuf, 512) != 0)
+		    do_log("[%s] %d fail: not zero\n", msg, (int)sector);
+	    }
+	    else {
+//		printf("[%d] %08x vs %08x\n", (int)sector, sector_crc[sector], (uint32_t)crc32(0, ptr, 512));
+		unsigned crc1 = 0, crc2 = 0;
+		//assert(sector_crc[sector] == (uint32_t)crc32(0, ptr, 512));
+		if ((crc1 = sector_crc[sector]) != (crc2 = crc32(0, ptr, 512)))
+		    do_log("[%s] %d fail: %08x not %08x\n", msg, (int)sector,
+			   crc1, crc2);
+	    }
+	    sector++;
+	}
+    }
+}
+
+static void printaddr(sector_t sector, rbd_image *img) {
+    sector_t base = sector, limit = base+8;
+    char buf[1024], *p = buf;
+    auto [wm,wmap] = img->wcache->getmap2();
+    {
+	std::unique_lock lk(*wm);
+	p += sprintf(p, " w %ld: [", sector);
+	for (auto it = wmap->lookup(base);
+	     it != wmap->end() && it->base() < limit; it++) {
+	    auto [_b,_l,_p] = it->vals(base, limit+512);
+	    p += sprintf(p, " %ld+%ld->%ld", _b, _l-_b, _p);
+	}
+	p += sprintf(p, " ]");
+	do_log("%s\n", buf);
+    }
+    {
+	p = buf;
+	std::shared_lock lk(img->map_lock);
+	p += sprintf(p, " r %ld: [", sector);
+	for (auto it = img->map.lookup(base);
+	     it != img->map.end() && it->base() < limit; it++) {
+	    auto [_b,_l,oo] = it->vals();
+	    p += sprintf(p, " %ld+%ld->%ld.%d", _b, _l-_b, oo.obj, oo.offset);
+	}
+	p += sprintf(p, " ]");
+	do_log("%s\n", buf);
+    }
+}
+
+static size_t iovsum(const iovec *iov, int iovcnt) {
+    int sum = 0;
+    for (int i = 0; i < iovcnt; i++)
+	sum += iov[i].iov_len;
+    return sum;
+}
+
+#endif
