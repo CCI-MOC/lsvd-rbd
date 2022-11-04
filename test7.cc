3259ad21cf6a027ef4379afc0be71bf8d8e2fb2d
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-11-04 19:15:38 +0000

    updated test7 to add more parameters, check CRCs. Fails out with CRC errors after 500 or 1000 writes on a 500M volume

--- test7.cc
+++ test7.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <argp.h>
 
 #include <random>
 #include <chrono>
@@ -8,14 +9,27 @@
 namespace fs = std::experimental::filesystem;
 #include <iostream>
 #include <queue>
+#include <map>
+#include <mutex>
 
 #include "fake_rbd.h"
 #include "objects.h"
 #include "lsvd_types.h"
 
 std::mt19937_64 rng;
-char *rnd_data;
-const int max_sectors = 32768;
+
+struct cfg {
+    const char *cache_dir;
+    const char *obj_prefix;
+    int    run_len;
+    size_t window;
+    int    image_sectors;
+    float  read_fraction;
+    int    n_runs;
+    long   seed;
+    bool   reopen;
+};
+
 
 /* empirical fit to the pattern of writes in the ubuntu install,
  * except it has lots more writes in the 128..32768 sector range
@@ -43,6 +57,9 @@ int gen_lba(int max, int n) {
     return uni(rng) * 8;
 }
 
+char *rnd_data;
+const int max_sectors = 32768;
+
 void init_random(void) {
     size_t bytes = max_sectors * 512;
     rnd_data = (char*)malloc(bytes);
@@ -59,21 +76,28 @@ void get_random(char *buf, int lba, int sectors) {
 	*p++ = lba++;
 }
 
-
 void clean_image(std::string name) {
     auto dir = fs::path(name).parent_path();
     std::string prefix = fs::path(name).filename();
-    
-    for (auto const& dir_entry : fs::directory_iterator{dir}) {
-	std::string entry{dir_entry.path().filename()};
-	if (strncmp(entry.c_str(), prefix.c_str(), prefix.size()) == 0)
-	    fs::remove(dir_entry.path());
+
+    if (!fs::exists(dir))
+	fs::create_directory(dir);
+    else {
+	for (auto const& dir_entry : fs::directory_iterator{dir}) {
+	    std::string entry{dir_entry.path().filename()};
+	    if (strncmp(entry.c_str(), prefix.c_str(), prefix.size()) == 0)
+		fs::remove(dir_entry.path());
+	}
     }
 }
 
-void clean_cache(std::string cache_dir, std::string name) {
-    auto path = cache_dir + "/" + name + ".cache";
-    unlink(path.c_str());
+void clean_cache(std::string cache_dir) {
+    const char *suffix = ".cache";
+    for (auto const& dir_entry : fs::directory_iterator{cache_dir}) {
+	    std::string entry{dir_entry.path().filename()};
+	    if (!strcmp(suffix, entry.c_str() + entry.size() - strlen(suffix)))
+		fs::remove(dir_entry.path());
+    }
 }
 
 void create_image(std::string name, int sectors) {
@@ -98,10 +122,41 @@ void create_image(std::string name, int sectors) {
     fclose(fp);
 }
 
-auto image_name = "/tmp/bkt/obj";
-    
-void drain(std::queue<std::pair<rbd_completion_t,char*>> &q, int window) {
-    while (q.size() >= window) {
+#include <zlib.h>
+static std::map<int,uint32_t> sector_crc;
+char _zbuf[512];
+static std::mutex zm;
+
+void add_crc(sector_t sector, const char *_buf, size_t bytes) {
+    auto buf = (const unsigned char *)_buf;
+    std::unique_lock lk(zm);
+    for (size_t i = 0; i < bytes; i += 512) {
+	const unsigned char *ptr = buf + i;
+	sector_crc[sector] = (uint32_t)crc32(0, ptr, 512);
+	sector++;
+    }
+}
+
+void check_crc(sector_t sector, const char *_buf, size_t bytes) {
+    auto buf = (const unsigned char *)_buf;
+    std::unique_lock lk(zm);
+    for (size_t i = 0; i < bytes; i += 512) {
+	const unsigned char *ptr = buf + i;
+	if (sector_crc.find(sector) == sector_crc.end()) {
+	    assert(memcmp(ptr, _zbuf, 512) == 0);
+	}
+	else {
+	    unsigned crc1 = 0, crc2 = 0;
+	    assert((crc1 = sector_crc[sector]) == (crc2 = crc32(0, ptr, 512)));
+	}
+	sector++;
+    }
+}
+
+typedef std::pair<rbd_completion_t,char*> opinfo;
+
+void drain(std::queue<opinfo> &q, size_t window) {
+    while (q.size() > window) {
 	auto [c,ptr] = q.front();
 	q.pop();
 	rbd_aio_wait_for_complete(c);
@@ -109,64 +164,165 @@ void drain(std::queue<std::pair<rbd_completion_t,char*>> &q, int window) {
 	free(ptr);
     }
 }
-    
-void run_test(unsigned long seed) {
+
+void run_test(unsigned long seed, struct cfg *cfg) {
     printf("seed: 0x%lx\n", seed);
+    rng.seed(seed);
 
-    //int image_sectors = 10*1024*1024*2; // 10G
-    int image_sectors = 1024*1024*2; // 1G
+    init_random();
 
-    clean_cache("/tmp", "obj");
-    clean_image(image_name);
-    create_image(image_name, image_sectors);
+    clean_cache(cfg->cache_dir);
+    clean_image(cfg->obj_prefix);
+    create_image(cfg->obj_prefix, cfg->image_sectors);
 
-    setenv("LSVD_CACHE_SIZE", "300M", 1);
+    setenv("LSVD_CACHE_SIZE", "100M", 1);
     setenv("LSVD_BACKEND", "file", 1);
+    setenv("LSVD_CACHE_DIR", cfg->cache_dir, 1);
     
     rados_ioctx_t io = 0;
     rbd_image_t img;
 
-    if (rbd_open(io, image_name, &img, NULL) < 0)
+    if (rbd_open(io, cfg->obj_prefix, &img, NULL) < 0)
 	printf("failed: rbd_open\n"), exit(1);
 
     std::queue<std::pair<rbd_completion_t,char*>> q;
-    int window = 16;
-    int n_writes = 10000;
-
-    for (int i = 0; i < n_writes; i++) {
-	drain(q, window);
+    std::uniform_real_distribution<> uni(0.0,1.0);
+    
+    for (int i = 0; i < cfg->run_len; i++) {
+	drain(q, cfg->window-1);
 	if (i % 1000 == 999)
 	    printf("+"), fflush(stdout);
 	rbd_completion_t c;
 	rbd_aio_create_completion(NULL, NULL, &c);
 
 	int n = n_sectors();
-	int lba = gen_lba(image_sectors, n);
+	int lba = gen_lba(cfg->image_sectors, n);
 	auto ptr = (char*)aligned_alloc(512, n*512);
 
 	q.push(std::make_pair(c, ptr));
-	rbd_aio_write(img, 512L * lba, 512L * n, ptr, c);
+	if (uni(rng) < cfg->read_fraction) {
+	    rbd_aio_read(img, 512L * lba, 512L * n, ptr, c);
+	}
+	else {
+	    get_random(ptr, lba, n);
+	    rbd_aio_write(img, 512L * lba, 512L * n, ptr, c);
+	    add_crc(lba, ptr, n*512L);
+	}
     }
     drain(q, 0);
     printf("\ndone\n");
+
+    if (cfg->reopen) {
+	rbd_close(img);
+	if (rbd_open(io, cfg->obj_prefix, &img, NULL) < 0)
+	    printf("failed: rbd_open\n"), exit(1);
+    }
+
+    auto buf = (char*)aligned_alloc(512, 64*1024);
+    for (int sector = 0; sector < cfg->image_sectors; sector += 64*2) {
+	rbd_read(img, sector*512L, 64*1024, buf);
+	check_crc(sector, buf, 64*1024);
+    }
+    free(buf);
     rbd_close(img);
 }
 
 
+static char args_doc[] = "RUNS";
+
+static struct argp_option options[] = {
+    {"seed",     's', "S",    0, "use this seed (one run)"},
+    {"len",      'l', "N",    0, "run length"},
+    {"window",   'w', "W",    0, "write window"},
+    {"size",     'z', "S",    0, "volume size (e.g. 1G, 100M)"},
+    {"cache-dir",'d', "DIR",  0, "cache directory"},
+    {"prefix",   'p', "PREFIX", 0, "object prefix"},
+    {"reads",    'r', "FRAC", 0, "fraction reads (0.0-1.0)"},
+    {"close",    'c', 0,      0, "close and re-open"},
+    {0},
+};
+
+struct cfg _cfg = {
+    "/tmp",			// cache_dir
+    "/tmp/bkt/obj",		// obj_prefix
+    10000, 			// run_len
+    16,				// window
+    1024*1024*2,		// image_sectors,
+    0.0,			// read_fraction
+    1,				// n_runs
+    0,				// seed
+    false};			// reopen
+
+off_t parseint(char *s)
+{
+    off_t val = strtol(s, &s, 0);
+    if (toupper(*s) == 'G')
+        val *= (1024*1024*1024);
+    if (toupper(*s) == 'M')
+        val *= (1024*1024);
+    if (toupper(*s) == 'K')
+        val *= 1024;
+    return val;
+}
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+    switch (key) {
+    case ARGP_KEY_INIT:
+	break;
+    case ARGP_KEY_ARG:
+        _cfg.n_runs = atoi(arg);
+        break;
+    case 's':
+	_cfg.seed = strtol(arg, NULL, 0);
+	break;
+    case 'l':
+	_cfg.run_len = atoi(arg);
+	break;
+    case 'w':
+	_cfg.window = atoi(arg);
+	break;
+    case 'z':
+	_cfg.image_sectors = parseint(arg) / 512;
+	break;
+    case 'd':
+	_cfg.cache_dir = arg;
+	break;
+    case 'p':
+	_cfg.obj_prefix = arg;
+	break;
+    case 'r':
+	_cfg.read_fraction = atof(arg);
+	break;
+    case 'c':
+	_cfg.reopen = true;
+	break;
+    case ARGP_KEY_END:
+        break;
+    }
+    return 0;
+}
+static struct argp argp = { options, parse_opt, NULL, args_doc};
+
 int main(int argc, char **argv) {
-    auto now = std::chrono::system_clock::now();
-    auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
-    auto value = now_ms.time_since_epoch();
-    long seed = value.count();
+    argp_parse (&argp, argc, argv, 0, 0, 0);
 
-    rng.seed(seed);
+    if (_cfg.seed) {
+	run_test(_cfg.seed, &_cfg);
+    }
+    else {
+	auto now = std::chrono::system_clock::now();
+	auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+	auto value = now_ms.time_since_epoch();
+	long seed = value.count();
 
-    clean_image("/tmp/bkt/obj");
-    
-    std::vector<unsigned long> seeds;
-    for (int i = 0; i < 20; i++)
-	seeds.push_back(rng());
-    for (auto s : seeds)
-	run_test(s);
+	rng.seed(seed);
+
+	std::vector<unsigned long> seeds;
+	for (int i = 0; i < _cfg.n_runs; i++)
+	    seeds.push_back(rng());
+	for (auto s : seeds)
+	    run_test(s, &_cfg);
+    }
 }
     
