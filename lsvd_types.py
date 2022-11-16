7aed281fbed6c21a7a8b112cb22f792cb44e9ce6
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-11-16 16:11:15 +0000

    add crc, write cache seq; delete ckpts

--- lsvd_types.py
+++ lsvd_types.py
@@ -21,8 +21,9 @@ class hdr(Structure):
                 ("type",                c_uint),
                 ("seq",                 c_uint),
                 ("hdr_sectors",         c_uint),
-                ("data_sectors",        c_uint)]
-sizeof_hdr = sizeof(hdr) # 40
+                ("data_sectors",        c_uint),
+                ("crc",                 c_uint)]
+sizeof_hdr = sizeof(hdr) # 44
 
 class super_hdr(Structure):
     _fields_ = [("vol_size",            c_ulong),
@@ -53,12 +54,12 @@ class snap(Structure):
 sizeof_snap = sizeof(snap) # 5
 
 class data_hdr(Structure):
-    _fields_ = [("last_data_obj",       c_uint),
+    _fields_ = [("cache_seq",           c_ulong),
                 ("objs_cleaned_offset", c_uint),
                 ("objs_cleaned_len",    c_uint),
                 ("map_offset",          c_uint),
                 ("map_len",             c_uint)]
-sizeof_data_hdr = sizeof(data_hdr) # 20
+sizeof_data_hdr = sizeof(data_hdr) # 24
 
 class obj_cleaned(Structure):
     _fields_ = [("seq",                 c_uint),
--- parse.py
+++ parse.py
@@ -77,6 +77,7 @@ if h.type == lsvd.LSVD_SUPER:
     print('seq:      ', h.seq)
     print('n_hdr:    ', h.hdr_sectors)
     print('n_data:   ', h.data_sectors)
+    print('crc:      ', '%08x' % h.crc)
 
     print('vol_size:      ', sh.vol_size)
     print('total_sectors: ', sh.total_sectors)
@@ -92,10 +93,7 @@ if h.type == lsvd.LSVD_SUPER:
 elif h.type == lsvd.LSVD_DATA:
     o3 = o2+lsvd.sizeof_data_hdr
     dh = lsvd.data_hdr.from_buffer(bytearray(obj[o2:o3]))
-    
-    o4 = dh.ckpts_offset; l4 = dh.ckpts_len
-    ckpts = (c_uint * (l4//4)).from_buffer(bytearray(obj[o4:o4+l4]))
-    
+        
     o5 = dh.objs_cleaned_offset; l5 = dh.objs_cleaned_len
     objs = (lsvd.obj_cleaned * (l5//lsvd.sizeof_obj_cleaned)).from_buffer(bytearray(obj[o5:o5+l5]))
 
@@ -109,14 +107,14 @@ elif h.type == lsvd.LSVD_DATA:
     print('seq:      ', h.seq)
     print('n_hdr:    ', h.hdr_sectors)
     print('n_data:   ', h.data_sectors)
+    print('crc:      ', '%08x' % h.crc)
 
-    print('last_data:', dh.last_data_obj, "(0x%x)" % dh.last_data_obj)
     print('cleaned:  ', dh.objs_cleaned_offset, ':', ', '.join(fmt_obj_cleaned(objs)))
     if args.nowrap:
             print('map:')
             print(' ' + '\n '.join(fmt_data_map(maps)))
     else:
-        print('map:      ', dh.map_offset, ':', ', '.join(fmt_data_map(maps)))
+        print('map:      ', '%d+%d' % (dh.map_offset,dh.map_len), ':', ', '.join(fmt_data_map(maps)))
     
 elif h.type == lsvd.LSVD_CKPT:
     o3 = o2+lsvd.sizeof_ckpt_hdr
@@ -131,7 +129,10 @@ elif h.type == lsvd.LSVD_CKPT:
         objs_txt = 'OBJECT TOO SHORT (%d bytes)' % len(obj)
     else:
         objs = (lsvd.ckpt_obj * (l5//lsvd.sizeof_ckpt_obj)).from_buffer(bytearray(obj[o5:o5+l5]))
-        objs_txt = ', '.join(fmt_obj(objs))
+        if args.nowrap:
+            objs_txt = '\n ' + '\n '.join(fmt_obj(objs))
+        else:
+            objs_txt = ', '.join(fmt_obj(objs))
 
 #    o6 = ch.deletes_offset; l6 = ch.deletes_len
 #    dels = (lsvd.deferred_delete * (l5//lsvd.sizeof_deferred_delete)).from_buffer(bytearray(obj[o6:o6+l6]))
@@ -142,7 +143,7 @@ elif h.type == lsvd.LSVD_CKPT:
     else:
         maps = (lsvd.ckpt_mapentry * (l7//lsvd.sizeof_ckpt_mapentry)).from_buffer(bytearray(obj[o7:o7+l7]))
         if args.nowrap:
-            map_txt = '\n ' + ' \n'.join(fmt_ckpt_map(maps))
+            map_txt = '\n ' + '\n '.join(fmt_ckpt_map(maps))
         else:
             map_txt = ', '.join(fmt_ckpt_map(maps))
 
@@ -153,7 +154,8 @@ elif h.type == lsvd.LSVD_CKPT:
     print('seq:      ', h.seq)
     print('n_hdr:    ', h.hdr_sectors)
     print('n_data:   ', h.data_sectors)
-
+    print('crc:      ', '%08x' % h.crc)
+    
     print('ckpts:    ', ch.ckpts_offset, ':', ', '.join(fmt_ckpt(ckpts)))
     print('objs:     ', ch.objs_offset, ':', objs_txt)
     print('map:      ', ch.map_offset, ':', map_txt)
