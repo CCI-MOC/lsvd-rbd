aa098d9e89bae386aa0e0fb98a93c742cb973f8f
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-11-17 20:15:57 +0000

    added options: --keep (leave data in disk between runs, not fully working yet), --sleep (child prints pid and waits 10s for debugging)

--- test10.cc
+++ test10.cc
@@ -4,6 +4,7 @@
 #include <argp.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <zlib.h>
 
 #include <random>
 #include <chrono>
@@ -48,6 +49,8 @@ struct cfg {
     int    lose_objs;
     bool   wipe_cache;
     size_t lose_writes;
+    bool   wipe_data;
+    bool   sleep;
 };
 
 
@@ -103,25 +106,6 @@ void get_random(char *buf, int lba, int sectors, int seq) {
     }
 }
 
-void clean_image(std::string name) {
-    auto dir = fs::path(name).parent_path();
-    std::string prefix = fs::path(name).filename();
-
-    if (!fs::exists(dir))
-	fs::create_directory(dir);
-    else {
-	for (auto const& dir_entry : fs::directory_iterator{dir}) {
-	    std::string entry{dir_entry.path().filename()};
-	    if (strncmp(entry.c_str(), prefix.c_str(), prefix.size()) == 0)
-		fs::remove(dir_entry.path());
-	    if (strcmp(".x", entry.c_str() + entry.size() - 2) == 0)
-		fs::remove(dir_entry.path());
-	    if (strncmp("gc.", entry.c_str(), 3))
-		fs::remove(dir_entry.path());
-	}
-    }
-}
-
 void clean_cache(std::string cache_dir) {
     const char *suffix = ".cache";
     for (auto const& dir_entry : fs::directory_iterator{cache_dir}) {
@@ -132,39 +116,6 @@ void clean_cache(std::string cache_dir) {
 	    fs::remove(dir_entry.path());
     }
 }
-
-void create_image(std::string name, int sectors) {
-    char buf[4096];
-    memset(buf, 0, sizeof(buf));
-    auto _hdr = (obj_hdr*) buf;
-
-    *_hdr = (obj_hdr){LSVD_MAGIC,
-		      1,	  // version
-		      {0},	  // UUID
-		      LSVD_SUPER, // type
-		      0,	  // seq
-		      8,	  // hdr_sectors
-		      0};	  // data_sectors
-    uuid_generate_random(_hdr->vol_uuid);
-
-    auto _super = (super_hdr*)(_hdr + 1);
-    *_super = (super_hdr){(uint64_t)sectors, // vol_size
-			  0,	   // total data sectors
-			  0,	   // live sectors
-			  1,	   // next object
-			  0, 0,	   // checkpoint offset, len
-			  0, 0,	   // clone offset, len
-			  0, 0};   // snap offset, len
-
-    unlink(name.c_str());
-    FILE *fp = fopen(name.c_str(), "w");
-    if (fp == NULL)
-	perror("image create");
-    fwrite(buf, sizeof(buf), 1, fp);
-    fclose(fp);
-}
-
-#include <zlib.h>
 
 
 typedef std::pair<rbd_completion_t,char*> opinfo;
@@ -353,191 +304,196 @@ void print_status(struct cfg *cfg) {
     close(fd);
 }
 
-bool started = false;
 extern char *p_log, *logbuf;
 
-void run_test(unsigned long seed, struct cfg *cfg) {
-    p_log = logbuf;
-    if (p_log)
-	*p_log = 0;
-    printf("seed: 0x%lx\n", seed);
-    rng.seed(seed);
+struct triple {
+    int lba;
+    int seq;
+    uint32_t crc;
+};
 
-    init_random();
-
-    if (!started || cfg->restart) {
-	clean_cache(cfg->cache_dir);
-	clean_image(cfg->obj_prefix);
-	create_image(cfg->obj_prefix, cfg->image_sectors);
-	started = true;
-    }
-
-    std::uniform_real_distribution<> uni(0.0,1.0);
-    int n_requests = cfg->run_len + (uni(rng)-0.5)*100;
-    int seq = 0;
-    
-    std::vector<std::tuple<int,int>> io_list;
-
-    for (int i = 0; i < n_requests; i++) {
-	int n = n_sectors();
-	int lba = gen_lba(cfg->image_sectors, n);
-	io_list.push_back(std::make_tuple(lba, n));
-    }
-
+void run_test(struct cfg *cfg) {
     rados_ioctx_t io = 0;
     rbd_image_t img;
+    bool started = false;
+    int seq = 0;
+    std::vector<triple> should_be_crcs(cfg->image_sectors);
+
     setenv("LSVD_CACHE_SIZE", cfg->cache_size, 1);
     setenv("LSVD_BACKEND", "file", 1);
     setenv("LSVD_CACHE_DIR", cfg->cache_dir, 1);
-
-    struct op {
-	int sector;
-	int len;
-	int seq;
-	std::vector<uint32_t> *crcs;
-    };
-    std::vector<op> op_crcs;
     
-    int pid = fork();
-    if (pid == 0) {
+    for (auto s : cfg->seeds) {
+	p_log = logbuf;
+	if (p_log)
+	    *p_log = 0;
+	printf("seed: 0x%lx\n", s);
+	rng.seed(s);
+
+	init_random();
+
+	if (!started || cfg->restart) {
+	    clean_cache(cfg->cache_dir);
+	    rbd_remove(io, cfg->obj_prefix);
+	    rbd_create(io, cfg->obj_prefix, cfg->image_sectors, NULL);
+	    started = true;
+	    seq = 0;
+	    for (int i = 0; i < cfg->image_sectors; i++)
+		should_be_crcs[i] = (triple){0, 0, 0xb2aa7578};
+	}
+
+	std::uniform_real_distribution<> uni(0.0,1.0);
+	int n_requests = cfg->run_len + (uni(rng)-0.5)*100;
+    
+	std::vector<std::tuple<int,int>> io_list;
+
+	for (int i = 0; i < n_requests; i++) {
+	    int n = n_sectors();
+	    int lba = gen_lba(cfg->image_sectors, n);
+	    io_list.push_back(std::make_tuple(lba, n));
+	}
+
+	struct op {
+	    int sector;
+	    int len;
+	    int seq;
+	    std::vector<uint32_t> *crcs;
+	};
+    
+	int pid = fork();
+	if (pid == 0) {
+	    if (cfg->sleep) {
+		printf("pid %d\n", getpid());
+		sleep(10);
+	    }
+	    if (rbd_open(io, cfg->obj_prefix, &img, NULL) < 0)
+		printf("failed: rbd_open\n"), exit(1);
+
+	    std::queue<std::pair<rbd_completion_t,char*>> q;
+	    for (int i = 0; i < n_requests; i++) {
+		drain(q, cfg->window-1);
+		if (i % 1000 == 999)
+		    printf("+"), fflush(stdout);
+		rbd_completion_t c;
+		rbd_aio_create_completion(NULL, NULL, &c);
+
+		auto [lba, n] = io_list[i];
+		auto ptr = (char*)aligned_alloc(512, n*512);
+	
+		q.push(std::make_pair(c, ptr));
+		if (uni(rng) < cfg->read_fraction) {
+		    rbd_aio_read(img, 512L * lba, 512L * n, ptr, c);
+		}
+		else {
+		    auto s = ++seq;
+		    get_random(ptr, lba, n, s);
+		    rbd_aio_write(img, 512L * lba, 512L * n, ptr, c);
+		}
+	    }
+	    printf(" K\n");
+	    _exit(0);
+	}
+
+	/* this relies on the random number stream being identical in 
+	 * the parent and child processes
+	 */
+	std::vector<op> op_crcs;
+	for (int i = 0; i < n_requests; i++) {
+	    if (uni(rng) < cfg->read_fraction)
+		continue;
+	    auto [lba, n] = io_list[i];
+	    auto ptr = (char*)malloc(n*512);
+	    auto s = ++seq;
+	    get_random(ptr, lba, n, s);
+
+	    auto crc_list = new std::vector<uint32_t>();
+	    for (size_t j = 0; j < 512UL*n; j += 512) {
+		auto p = (const unsigned char*)ptr + j;
+		auto crc = (uint32_t)crc32(0, p, 512);
+		crc_list->push_back(crc);
+	    }
+	    op_crcs.push_back((op){lba, n, s, crc_list});
+	    free(ptr);
+	}
+
+	int status;
+	waitpid(pid, &status, 0);
+	print_status(cfg);
+    
+	if (cfg->lose_objs)
+	    obj_delete(cfg);
+	if (cfg->wipe_cache)
+	    clean_cache(cfg->cache_dir);
+	else if (cfg->lose_writes)
+	    lose_writes(cfg);
+
+	usleep(100000);
 	if (rbd_open(io, cfg->obj_prefix, &img, NULL) < 0)
 	    printf("failed: rbd_open\n"), exit(1);
 
-	std::queue<std::pair<rbd_completion_t,char*>> q;
-	for (int i = 0; i < n_requests; i++) {
-	    drain(q, cfg->window-1);
-	    if (i % 1000 == 999)
-		printf("+"), fflush(stdout);
-	    rbd_completion_t c;
-	    rbd_aio_create_completion(NULL, NULL, &c);
+	auto tmp = __lsvd_dbg_be_delay;
+	__lsvd_dbg_be_delay = false;
 
-	    auto [lba, n] = io_list[i];
-	    auto ptr = (char*)aligned_alloc(512, n*512);
-	
-	    q.push(std::make_pair(c, ptr));
-	    if (uni(rng) < cfg->read_fraction) {
-		rbd_aio_read(img, 512L * lba, 512L * n, ptr, c);
+	const int bufsize = 128*1024;
+	auto buf = (char*)aligned_alloc(512, bufsize);
+	std::vector<triple> image_info(cfg->image_sectors);
+    
+	int i = 0;
+	for (int sector = 0; sector < cfg->image_sectors; sector += bufsize/512) {
+	    memset(buf, 0, bufsize);
+	    rbd_read(img, sector*512L, bufsize, buf);
+	    for (int j = 0; j < bufsize; j += 512) {
+		auto p = (const unsigned char*)buf + j;
+		uint32_t crc = crc32(0, p, 512);
+		image_info[sector + j/512] = (triple){*(int*)p,
+						      *(int*)(p+4), crc};
+		assert(*(int*)(p+4) <= (int)image_info.size()+1);
 	    }
-	    else {
-		auto s = ++seq;
-		get_random(ptr, lba, n, s);
-		rbd_aio_write(img, 512L * lba, 512L * n, ptr, c);
+	    if (++i > cfg->image_sectors / (bufsize/512) / 10) {
+		printf("-"); fflush(stdout);
+		i = 0;
 	    }
 	}
-	printf(" K\n");
-	_exit(0);
-    }
-
-    /* this relies on the random number stream being identical in 
-     * the parent and child processes
-     */
-    std::vector<bool> bitmap(cfg->image_sectors);
-    for (int i = 0; i < n_requests; i++) {
-	if (uni(rng) < cfg->read_fraction)
-	    continue;
-	auto [lba, n] = io_list[i];
-	auto ptr = (char*)malloc(n*512);
-	auto s = ++seq;
-	get_random(ptr, lba, n, s);
-
-	auto crc_list = new std::vector<uint32_t>();
-	for (size_t j = 0; j < 512UL*n; j += 512) {
-	    auto p = (const unsigned char*)ptr + j;
-	    auto crc = (uint32_t)crc32(0, p, 512);
-	    crc_list->push_back(crc);
-	    bitmap[lba+j/512] = true;
-	}
-	op_crcs.push_back((op){lba, n, s, crc_list});
-	free(ptr);
-    }
-
-    int status;
-    waitpid(pid, &status, 0);
-    print_status(cfg);
+	printf("\n");
     
-    if (cfg->lose_objs)
-	obj_delete(cfg);
-    if (cfg->wipe_cache)
-	clean_cache(cfg->cache_dir);
-    else if (cfg->lose_writes)
-	lose_writes(cfg);
+	int max_seq = 0;
+	for (int i = 0; i < cfg->image_sectors; i++)
+	    if (image_info[i].seq > max_seq) {
+		assert(max_seq <= (int)image_info.size()+1);
+		max_seq = image_info[i].seq;
+	    }
 
-    usleep(100000);
-    if (rbd_open(io, cfg->obj_prefix, &img, NULL) < 0)
-	printf("failed: rbd_open\n"), exit(1);
-
-    auto tmp = __lsvd_dbg_be_delay;
-    __lsvd_dbg_be_delay = false;
-
-    struct triple {
-	int lba;
-	int seq;
-	uint32_t crc;
-    };
-
-    const int bufsize = 128*1024;
-    auto buf = (char*)aligned_alloc(512, bufsize);
-    std::vector<triple> image_info(cfg->image_sectors);
+	printf("max_seq = %d\n", max_seq);
+	int secs = 0;
+	for (size_t i = max_seq; i < op_crcs.size(); i++)
+	    secs += op_crcs[i].len;
+	printf("lost %d writes (%.1f MB)\n", n_requests - max_seq, secs / 2.0 / 1024);
+	assert(cfg->lose_writes || (n_requests - max_seq) < (int)cfg->window);
     
-    int i = 0;
-    for (int sector = 0; sector < cfg->image_sectors; sector += bufsize/512) {
-	memset(buf, 0, bufsize);
-	rbd_read(img, sector*512L, bufsize, buf);
-	for (int j = 0; j < bufsize; j += 512) {
-	    auto p = (const unsigned char*)buf + j;
-	    uint32_t crc = crc32(0, p, 512);
-	    image_info[sector + j/512] = (triple){*(int*)p,
-						  *(int*)(p+4), crc};
-	    assert(*(int*)(p+4) <= (int)image_info.size()+1);
+	for (int i = 0; i < max_seq; i++) {
+	    auto [lba,len,seq,crcs] = op_crcs[i];
+	    assert(seq == i+1);
+	    int j = 0;
+	    for (auto c : *crcs) {
+		should_be_crcs[lba+j] = (triple){lba+j,seq,c};
+		j++;
+	    }
 	}
-	if (++i > cfg->image_sectors / (bufsize/512) / 10) {
-	    printf("-"); fflush(stdout);
-	    i = 0;
-	}
+	int failed = 0;
+	for (int i = 0; i < cfg->image_sectors && failed < 100; i++)
+	    if (should_be_crcs[i].crc != image_info[i].crc) {
+		printf("%d : %08x (seq %d) should be: %08x (%d)\n", i,
+		       image_info[i].crc, image_info[i].seq,
+		       should_be_crcs[i].crc, should_be_crcs[i].seq);
+		failed++;
+	    }
+	assert(!failed);
+	printf("ok\n");
+
+	free(buf);
+	rbd_close(img);
+	__lsvd_dbg_be_delay = tmp;
     }
-    printf("\n");
-    
-    int max_seq = 0;
-    for (int i = 0; i < cfg->image_sectors; i++)
-	if (image_info[i].seq > max_seq) {
-	    assert(max_seq <= (int)image_info.size()+1);
-	    max_seq = image_info[i].seq;
-	}
-
-    printf("max_seq = %d\n", max_seq);
-    int secs = 0;
-    for (size_t i = max_seq; i < op_crcs.size(); i++)
-	secs += op_crcs[i].len;
-    printf("lost %d writes (%.1f MB)\n", n_requests - max_seq, secs / 2.0 / 1024);
-    assert(cfg->lose_writes || (n_requests - max_seq) < (int)cfg->window);
-    
-    std::vector<triple> should_be_crcs(cfg->image_sectors);
-    for (int i = 0; i < cfg->image_sectors; i++)
-	should_be_crcs[i].crc = 0xb2aa7578;
-
-    for (int i = 0; i < max_seq; i++) {
-	auto [lba,len,seq,crcs] = op_crcs[i];
-	assert(seq == i+1);
-	int j = 0;
-	for (auto c : *crcs) {
-	    should_be_crcs[lba+j] = (triple){lba+j,seq,c};
-	    j++;
-	}
-    }
-    int failed = 0;
-    for (int i = 0; i < cfg->image_sectors && failed < 100; i++)
-	if (should_be_crcs[i].crc != image_info[i].crc) {
-	    printf("%d : %08x (seq %d) should be: %08x (%d)\n", i,
-		   image_info[i].crc, image_info[i].seq,
-		   should_be_crcs[i].crc, should_be_crcs[i].seq);
-	    failed++;
-	}
-    assert(!failed);
-    printf("ok\n");
-
-    free(buf);
-    rbd_close(img);
-    __lsvd_dbg_be_delay = tmp;
 }
 
 
@@ -560,6 +516,8 @@ static struct argp_option options[] = {
     {"wipe-cache",'W', 0,     0, "delete cache on restart"},
     {"lose-writes",'L', "N",    0, "delete some of last N cache writes"},
     {"cache-size",'Z', "N",    0, "cache size (K/M/G)"},
+    {"no-wipe",  'n', 0,      0, "don't clear image between runs"},
+    {"sleep",    'S', 0,      0, "child sleeps for debug attach"},
     {0},
 };
 
@@ -578,7 +536,9 @@ struct cfg _cfg = {
     false,			// existing
     0,				// lose_objs
     false,			// wipe_cache
-    0};				// lose_writes
+    0,				// lose_writes
+    true,			// wipe_data
+    false};			// sleep
 
 off_t parseint(char *s)
 {
@@ -648,6 +608,12 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
     case 'Z':
 	_cfg.cache_size = arg;
 	break;
+    case 'n':
+	_cfg.wipe_data = false;
+	break;
+    case 'S':
+	_cfg.sleep = true;
+	break;
     case ARGP_KEY_END:
         break;
     }
@@ -659,24 +625,17 @@ int main(int argc, char **argv) {
     argp_parse (&argp, argc, argv, 0, 0, 0);
     __lsvd_dbg_rename = true;
     
-    if (_cfg.seeds.size() > 0) {
-	for (int i = 0; i < _cfg.n_runs; i++)
-	    for (auto s : _cfg.seeds)
-		run_test(s, &_cfg);
-    }
-    else {
+    if (_cfg.seeds.size() == 0) {
 	auto now = std::chrono::system_clock::now();
 	auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
 	auto value = now_ms.time_since_epoch();
 	long seed = value.count();
 
 	rng.seed(seed);
-
-	std::vector<unsigned long> seeds;
 	for (int i = 0; i < _cfg.n_runs; i++)
-	    seeds.push_back(rng());
-	for (auto s : seeds)
-	    run_test(s, &_cfg);
+	    _cfg.seeds.push_back(rng());
     }
+
+    run_test(&_cfg);
 }
     
