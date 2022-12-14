17f52a9e1e0e6eb78e33376aeff9e1b75419f97e
Peter Desnoyers <p.desnoyers@northeastern.edu>
2022-12-14 15:05:18 +0000

    add get_uuid function for rbd_remove

--- translate.h
+++ translate.h
@@ -57,5 +57,6 @@ extern translate *make_translate(backend *_io, lsvd_config *cfg,
 extern int translate_create_image(backend *objstore, const char *name,
                                   uint64_t size);
 extern int translate_remove_image(backend *objstore, const char *name);
+extern int translate_get_uuid(backend *objstore, const char *name, uuid_t &uu);
 
 #endif
