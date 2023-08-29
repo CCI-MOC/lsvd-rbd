79cb0de2271b1fd150eb98e77eba574df29b2177
Peter Desnoyers <p.desnoyers@northeastern.edu>
2023-08-29 02:30:36 +0000

    some changes, but some stuff commented out

--- experiments/bench-lsvd.bash
+++ experiments/bench-lsvd.bash
@@ -7,7 +7,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-if [ -z "$1" ]
+if [ -z "${1:-}" ]
   then echo "Please provide a pool name"
   exit
 fi
@@ -16,9 +16,9 @@ fi
 pool_name=$1
 
 cur_time=$(date +"%FT%T")
-spdk_dir=/home/isaackhor/code/spdk/
-lsvd_dir=/home/isaackhor/code/lsvd-rbd/
-experiment_dir=/home/isaackhor/code/lsvd-rbd/experiments/
+spdk_dir=/home/pjd/spdk/
+lsvd_dir=/home/pjd/new-stuff/
+experiment_dir=/home/pjd/new-stuff/experiments/
 results_dir=$experiment_dir/results/
 outfile=$experiment_dir/results/$cur_time.$pool_name.lsvd.txt
 
@@ -31,9 +31,12 @@ cd $lsvd_dir
 make -j20 release
 # make -j10 debug
 make imgtool
+make thick-image
 
+IMG=$cur_time
 # Create the image
-./imgtool --create --rados --size=10g $pool_name/$cur_time
+#./imgtool --create --rados --size=10g $pool_name/$cur_time
+./thick-image --size=10g $pool_name/$IMG
 #./imgtool --create --rados --size=10g rbd/fio-target
 
 # setup spdk
@@ -48,8 +51,11 @@ mkdir -p /mnt/nvme/lsvd-rcache /mnt/nvme/lsvd-wcache
 export LD_PRELOAD=$lsvd_dir/liblsvd.so
 export LSVD_RCACHE_DIR=/mnt/nvme/lsvd-rcache
 export LSVD_WCACHE_DIR=/mnt/nvme/lsvd-wcache
+export LSVD_GC_THRESHOLD=40
 scripts/rpc.py spdk_kill_instance SIGTERM > /dev/null || true
 pkill nvmf_tgt || true
+pkill reactor_0 || true
+sleep 5
 ./build/bin/nvmf_tgt &
 
 # == setup spdk target ===
@@ -59,7 +65,7 @@ printf "\n\n\n===Starting automated test on pool: $pool_name===\n\n" >> $outfile
 scripts/rpc.py bdev_rbd_register_cluster rbd_cluster
 # syntax: bdev_rbd_create <name> <pool> <image-name> <block_size> [-c <cluster_name>]
 # image must already exist
-scripts/rpc.py bdev_rbd_create $pool_name $pool_name/$cur_time 4096 -c rbd_cluster
+scripts/rpc.py bdev_rbd_create $pool_name $pool_name/$IMG 4096 -c rbd_cluster
 
 # create subsystem
 scripts/rpc.py nvmf_create_subsystem nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001 -d SPDK_Controller1
@@ -70,12 +76,16 @@ scripts/rpc.py nvmf_create_transport -t TCP -u 16384 -m 8 -c 8192
 scripts/rpc.py nvmf_subsystem_add_listener nqn.2016-06.io.spdk:cnode1 -t tcp -a 10.1.0.5 -s 9922
 
 # == run benchmark and collect results ===
-cd $experiment_dir
-scp ./filebench-*.txt root@$client_host:/tmp/
-ssh $client_host "bash -s" < client-bench.bash 2>&1 | tee -a $outfile
+#cd $experiment_dir
+#scp ./filebench-*.txt root@$client_host:/tmp/
+#ssh $client_host "bash -s" < client-bench.bash 2>&1 | tee -a $outfile
+
+echo Hit ENTER to terminate...
+read a
 
 # cleanup
 cd $spdk_dir
 scripts/rpc.py bdev_rbd_delete Ceph0
 scripts/rpc.py bdev_rbd_unregister_cluster rbd_cluster
 scripts/rpc.py spdk_kill_instance SIGTERM
+sleep 5
