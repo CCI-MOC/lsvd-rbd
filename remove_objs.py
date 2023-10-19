7f5ee120d50e6f3237aa9b667cd7dc464ef5c985
Isaac Khor <s.github@isaackhor.com>
2023-10-19 21:40:52 +0000

    Add script to wipe all objects of an image throughly

---
+++ remove_objs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import rados, sys
+
+pool_name = sys.argv[1]
+img_name = sys.argv[2]
+
+if pool_name == '' or img_name == '':
+	print('Usage: remove_objs.py <pool_name> <image_name>')
+	exit(1)
+
+print(f'Removing all objects from pool {pool_name} for image {img_name}')
+
+cluster = rados.Rados(conffile='/etc/ceph/ceph.conf')
+cluster.connect()
+
+ioctx = cluster.open_ioctx('triple-ssd')
+
+num = 0
+removed = 0
+
+for obj in ioctx.list_objects():
+	num += 1
+	name = obj.key
+
+	if name.startswith('lsvd-benchmark'):
+		obj.remove()
+		removed += 1
+
+	if num % 1000 == 0:
+		print('+', end='', flush=True)
+
+print(f'\nRemoved {removed}/{num} objects')
+
+ioctx.close()
