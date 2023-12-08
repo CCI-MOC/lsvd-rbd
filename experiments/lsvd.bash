e410a950ebf40b87e8b2a682e0627ea1a76c01fb
sumatra-dhimoyee <sumatrad@bu.edu>
2023-12-08 15:19:46 +0000

    changes

---
+++ experiments/lsvd.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./bench-lsvd.bash triple-hdd
+./bench-lsvd.bash triple-ssd
+
+# something about parse-results.bash
---
+++ experiments/nightly_hdd.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+./bench-rbd.bash triple-hdd
+./bench-lsvd.bash triple-hdd
+# ./bench-ramdisk.bash
+
+# something about parse-results.bash
