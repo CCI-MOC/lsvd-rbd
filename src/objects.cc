b4b11f7629b5028443179d71d7404a89654e504c
Isaac Khor <s.github@isaackhor.com>
2024-05-20 23:05:44 +0000

    Overhaul backend parsing code

--- src/objects.cc
+++ src/objects.cc
@@ -1,122 +1,150 @@
-#include <string.h>
+#include <algorithm>
 #include <sys/uio.h>
+#include <utility>
 #include <zlib.h>
 
-#include "lsvd_debug.h"
 #include "lsvd_types.h"
 #include "objects.h"
+#include "utils.h"
 
-char *object_reader::read_object_hdr(const char *name, bool fast)
+void serialise_common_hdr(vec<byte> buf, obj_type t, seqnum_t s, u32 hdr,
+                          u32 data, uuid_t &uuid)
 {
-    obj_hdr *h = (obj_hdr *)malloc(4096);
-    int rv;
-    if ((rv = objstore->read(name, 0, h, 4096)) < 0)
-        goto fail;
-    if (fast)
-        return (char *)h;
-    if (h->hdr_sectors > 8) {
-        size_t len = h->hdr_sectors * 512;
-        h = (obj_hdr *)realloc(h, len);
-        if (objstore->read(name, 0, h, len) < 0)
-            goto fail;
-    }
-    return (char *)h;
-fail:
-    free((char *)h);
-    return NULL;
+    if (buf.size() < sizeof(common_obj_hdr))
+        buf.resize(sizeof(common_obj_hdr));
+
+    auto h = (common_obj_hdr *)buf.data();
+    *h = (common_obj_hdr){.magic = LSVD_MAGIC,
+                          .version = 1,
+                          .vol_uuid = {0},
+                          .type = t,
+                          .seq = s,
+                          .hdr_sectors = hdr,
+                          .data_sectors = data,
+                          .crc = 0};
+    uuid_copy(h->vol_uuid, uuid);
 }
 
-/* read all info from superblock, returns a vast number of things:
- * [super, vol_size] = f(name, &ckpts, &clones, *&snaps):
- *  - super - pointer to buffer (must be freed)
- *  - vol_size - in bytes (-1 on failure)
- *  - ckpts, clones, snaps - what you'd expect
- */
-std::pair<char *, ssize_t>
-object_reader::read_super(const char *name, std::vector<uint32_t> &ckpts,
-                          std::vector<clone_info *> &clones,
-                          std::vector<snap_info *> &snaps, uuid_t &uuid)
+void serialise_superblock(vec<byte> buf, vec<seqnum_t> &checkpoints,
+                          vec<clone_base> &clones, uuid_t &uuid)
 {
-    char *super_buf = read_object_hdr(name, false);
-    if (super_buf == NULL)
-        return std::make_pair((char *)NULL, -1);
+    usize req_size = sizeof(common_obj_hdr) + sizeof(super_hdr);
+    req_size += checkpoints.size() * sizeof(seqnum_t);
+    req_size = std::max(req_size, 8ul); // minimum of 4096 bytes
+    req_size = round_up(req_size, 512); // round to sector boundary
 
-    auto super_h = (obj_hdr *)super_buf;
+    if (buf.size() < req_size)
+        buf.resize(req_size);
 
-    if (super_h->magic != LSVD_MAGIC || super_h->version != 1 ||
-        super_h->type != LSVD_SUPER)
-        return std::make_pair((char *)NULL, -1);
-    memcpy(uuid, super_h->vol_uuid, sizeof(uuid_t));
+    serialise_common_hdr(buf, OBJ_SUPERBLOCK, 0, req_size / 512, 0, uuid);
 
-    super_hdr *super_sh = (super_hdr *)(super_h + 1);
+    auto h = (super_hdr *)(buf.data() + sizeof(common_obj_hdr));
+    h->ckpts_offset = sizeof(common_obj_hdr) + sizeof(super_hdr);
+    h->ckpts_len = checkpoints.size() * sizeof(seqnum_t);
 
-    decode_offset_len<uint32_t>(super_buf, super_sh->ckpts_offset,
-                                super_sh->ckpts_len, ckpts);
-    decode_offset_len_ptr<clone_info>(super_buf, super_sh->clones_offset,
-                                      super_sh->clones_len, clones);
-    decode_offset_len_ptr<snap_info>(super_buf, super_sh->snaps_offset,
-                                     super_sh->snaps_len, snaps);
-
-    return std::make_pair(super_buf, super_sh->vol_size * 512);
+    auto p = (seqnum_t *)(buf.data() + h->ckpts_offset);
+    for (auto &c : checkpoints)
+        *p++ = c;
 }
 
-/* read and decode the header of an object. Copies into arguments,
- * frees all allocated memory
- */
-ssize_t object_reader::read_data_hdr(const char *name, obj_hdr &h,
-                                     obj_data_hdr &dh,
-                                     std::vector<obj_cleaned> &cleaned,
-                                     std::vector<data_map> &dmap)
+opt<vec<byte>> object_reader::fetch_object_header(std::string objname)
 {
-    char *buf = read_object_hdr(name, false);
-    if (buf == NULL)
-        return -1;
-    auto tmp_h = (obj_hdr *)buf;
-    auto tmp_dh = (obj_data_hdr *)(tmp_h + 1);
-    if (tmp_h->type != LSVD_DATA) {
-        free(buf);
-        return -1;
-    }
+    vec<byte> buf(4096);
+    auto err = objstore->read(objname, 0, buf.data(), 4096);
+    RET_IF(err != 4096, std::nullopt);
 
-    h = *tmp_h;
-    dh = *tmp_dh;
+    auto h = (common_obj_hdr *)buf.data();
 
-    decode_offset_len<obj_cleaned>(buf, tmp_dh->objs_cleaned_offset,
-                                   tmp_dh->objs_cleaned_len, cleaned);
-    decode_offset_len<data_map>(buf, tmp_dh->data_map_offset,
-                                tmp_dh->data_map_len, dmap);
+    // Validate magic
+    PR_RET_IF(h->magic != LSVD_MAGIC || h->version != 1, std::nullopt,
+              "Invalid magic or version in object '{}'", objname);
 
-    free(buf);
-    return 0;
+    if (h->hdr_sectors <= 8)
+        return buf;
+
+    // Header is longer than 4096, we have to fetch the rest
+    auto len = h->hdr_sectors * 512;
+    buf.reserve(len);
+    err = objstore->read(objname, 0, buf.data(), len);
+    PR_ERR_RET_IF(std::cmp_not_equal(err, len), std::nullopt, err,
+                  "Failed to read object '{}' header", objname);
+
+    return buf;
 }
 
-/* read and decode a checkpoint object identified by sequence number
- */
-ssize_t object_reader::read_checkpoint(const char *name, uint64_t &cache_seq,
-                                       std::vector<uint32_t> &ckpts,
-                                       std::vector<ckpt_obj> &objects,
-                                       std::vector<deferred_delete> &deletes,
-                                       std::vector<ckpt_mapentry> &dmap)
+opt<parsed_superblock> object_reader::read_superblock(std::string oname)
 {
-    char *buf = read_object_hdr(name, false);
-    if (buf == NULL) {
-        do_log("buf == NULL\n");
-        return -1;
-    }
-    auto h = (obj_hdr *)buf;
-    auto ch = (obj_ckpt_hdr *)(h + 1);
-    if (h->type != LSVD_CKPT) {
-        do_log("%s: WRONG TYPE %d\n", name, h->type);
-        free(buf);
-        return -1;
-    }
-    cache_seq = ch->cache_seq;
-    decode_offset_len<uint32_t>(buf, ch->ckpts_offset, ch->ckpts_len, ckpts);
-    decode_offset_len<ckpt_obj>(buf, ch->objs_offset, ch->objs_len, objects);
-    decode_offset_len<deferred_delete>(buf, ch->deletes_offset, ch->deletes_len,
-                                       deletes);
-    decode_offset_len<ckpt_mapentry>(buf, ch->map_offset, ch->map_len, dmap);
+    auto buf = objstore->read_whole_obj(oname);
+    PASSTHRU_NULLOPT(buf);
+    auto hdr = (common_obj_hdr *)buf->data();
 
-    free(buf);
-    return 0;
+    PR_RET_IF(hdr->magic != LSVD_MAGIC, std::nullopt,
+              "Corrupt object; invalid magic at '{}'", oname);
+    PR_RET_IF(hdr->version != 1, std::nullopt,
+              "Invalid version in object '{}', only 1 is supported", oname);
+    PR_RET_IF(hdr->type != OBJ_SUPERBLOCK, std::nullopt,
+              "Obj '{}' not a superblock", oname);
+
+    parsed_superblock ret;
+    super_hdr *superblk = (super_hdr *)(hdr + 1);
+
+    decode_offset_len<u32>((char *)hdr, superblk->ckpts_offset,
+                           superblk->ckpts_len, ret.ckpts);
+    decode_offset_len_ptr<clone_info>((char *)hdr, superblk->clones_offset,
+                                      superblk->clones_len, ret.clones);
+    decode_offset_len_ptr<snap_info>((char *)hdr, superblk->snaps_offset,
+                                     superblk->snaps_len, ret.snaps);
+
+    ret.superblock_buf = *buf;
+    uuid_copy(ret.uuid, hdr->vol_uuid);
+    ret.vol_size = superblk->vol_size * 512;
+
+    return ret;
+}
+
+opt<parsed_data_hdr> object_reader::read_data_hdr(std::string oname)
+{
+    auto hdr = fetch_object_header(oname);
+    PASSTHRU_NULLOPT(hdr);
+
+    parsed_data_hdr h;
+    h.hdr = (common_obj_hdr *)h.buf.data();
+    PR_RET_IF(h.hdr->type != OBJ_LOGDATA, std::nullopt,
+              "Invalid object type in '{}'", oname);
+    h.data_hdr = (obj_data_hdr *)(hdr->data() + sizeof(common_obj_hdr));
+
+    auto buf = (char *)hdr->data();
+    decode_offset_len_ptr<obj_cleaned>(buf, h.data_hdr->objs_cleaned_offset,
+                                       h.data_hdr->objs_cleaned_len, h.cleaned);
+    decode_offset_len_ptr<data_map>(buf, h.data_hdr->data_map_offset,
+                                    h.data_hdr->data_map_len, h.data_map);
+
+    h.buf = std::move(*hdr);
+    return h;
+}
+
+opt<parsed_checkpoint> object_reader::read_checkpoint(std::string oname)
+{
+    auto hdr = fetch_object_header(oname);
+    PASSTHRU_NULLOPT(hdr);
+
+    parsed_checkpoint ret;
+    ret.hdr = (common_obj_hdr *)hdr->data();
+    PR_RET_IF(ret.hdr->type != OBJ_CHECKPOINT, std::nullopt,
+              "Invalid object type it '{}'", oname);
+    ret.ckpt_hdr = (obj_ckpt_hdr *)(&hdr->at(sizeof(common_obj_hdr)));
+
+    auto buf = (char *)hdr->data();
+    decode_offset_len<u32>(buf, ret.ckpt_hdr->ckpts_offset,
+                           ret.ckpt_hdr->ckpts_len, ret.ckpts);
+
+    decode_offset_len_ptr<ckpt_obj>(buf, ret.ckpt_hdr->objs_offset,
+                                    ret.ckpt_hdr->objs_len, ret.objects);
+    decode_offset_len_ptr<deferred_delete>(buf, ret.ckpt_hdr->deletes_offset,
+                                           ret.ckpt_hdr->deletes_len,
+                                           ret.deletes);
+    decode_offset_len_ptr<ckpt_mapentry>(buf, ret.ckpt_hdr->map_offset,
+                                         ret.ckpt_hdr->map_len, ret.dmap);
+    ret.buf = std::move(*hdr);
+    return ret;
 }
